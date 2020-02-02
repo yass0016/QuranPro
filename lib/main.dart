63b44ea3c935c925d5e906ac74310345bf276db2
Saleh Yassin <saleh@Salehs-iMac.local>
2020-02-01 20:54:56 -0500

    Fixed app not running on newer flutter versions

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:quranpro/src/app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
   String quranStyle = prefs.getString("quranStyle");
+
 
   if(quranStyle == null) {
     await prefs.setString('quranStyle', "quran_simple");
